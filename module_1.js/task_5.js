0d22cf63479fd3ffbdfe4675da4e53d70d4a8c3e
Yaroslav Kutsyi <yarik000315@gmail.com>
2020-01-24 00:41:30 +0200

    1

---
+++ module_1.js/task_5.js
@@ -0,0 +1,36 @@
+const china = 'китай';
+const chinaPrice = 100;
+const chile = 'чили';
+const chilePrice = 250;
+const australia = 'австралия';
+const australiaPrice = 170;
+const india = 'индия';
+const indiaPrice = 80;
+const jamaica = 'ямайка';
+const jamaicaPrice = 120;
+let userSelectedCountry = prompt('Введите страну доставки.');
+
+if (userSelectedCountry === null) {
+  console.log('ничего не введено');
+} else {
+  switch (userSelectedCountry.toLowerCase()) {
+    case china:
+      alert(`Доставка в ${china} будет стоить ${chilePrice} кредитов`);
+      break;
+    case chile:
+      alert(`Доставка в ${chile} будет стоить ${chilePrice} кредитов`);
+      break;
+    case australia:
+      alert(`Доставка в ${australia} будет стоить ${australiaPrice} кредитов`);
+      break;
+    case india:
+      alert(`Доставка в ${india} будет стоить ${indiaPrice} кредитов`);
+      break;
+    case jamaica:
+      alert(`Доставка в ${jamaica} будет стоить ${jamaicaPrice} кредитов`);
+      break;
+
+    default:
+      alert('В Вашей стране доставка не доступна');
+  }
+}
---
+++ module_1.js/task_6.js
@@ -0,0 +1,12 @@
+let input;
+let totalAmount = 0;
+
+while (input !== null) {
+  input = prompt(`Введите число!`);
+
+  if (isNaN(input)) {
+    alert('Введите число!');
+} else totalAmount = totalAmount + Number(input);
+}
+
+alert(`Общая сумма чисел равна ${totalAmount}`);
